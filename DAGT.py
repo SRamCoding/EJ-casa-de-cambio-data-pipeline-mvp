b02e40a2b8861c96c2864803aed27ae19b3a6090
SRamCoding <20rsebastian.trabajo@gmail.com>
2025-12-09 09:55:18 -0500

    commit

---
+++ DAGT.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+def extract():
+    print("Extrayendo data desde la fuente…")
+    # Aquí iría la lógica de lectura (S3, Mongo, API, etc.)
+
+def transform():
+    print("Transformando data con PySpark…")
+    # Aquí se ejecutará tu script PySpark (lo agregamos luego)
+
+def validate():
+    from validation.schema import validate_data  # Pandera schema
+    validate_data()
+    print("Validación completada.")
+
+def load():
+    print("Cargando data validada al destino…")
+
+default_args = {
+    "owner": "sebastian",
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="etl_diario",
+    default_args=default_args,
+    description="Pipeline ETL diario",
+    schedule_interval="@daily",
+    start_date=datetime(2026, 1, 1),
+    catchup=False
+) as dag:
+
+    t1 = PythonOperator(task_id="extract", python_callable=extract)
+    t2 = PythonOperator(task_id="transform", python_callable=transform)
+    t3 = PythonOperator(task_id="validate", python_callable=validate)
+    t4 = PythonOperator(task_id="load", python_callable=load)
+
+    t1 >> t2 >> t3 >> t4
--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 
-# Reemplaza <usuario>, <password>, <cluster> por tus datos
 MONGO_URI = "mongodb+srv://20RSebas:aerials2002@mongo-db-cluster.fqjwm4d.mongodb.net/?appName=mongo-db-cluster"
 
 client = MongoClient(MONGO_URI)
